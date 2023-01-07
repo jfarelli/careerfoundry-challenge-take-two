d9400a9b244ab4de823f0a567f9eb8a0c2dee4fa
Jordan Farelli <jordanfarelli@gmail.com>
2023-01-07 12:06:56 -0700

    created initial component files

---
+++ src/components/CourseContainer/CourseContainer.js
@@ -0,0 +1,11 @@
+import './CourseContainer.css'
+
+const CourseContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CourseContainer
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Loading
